a90e6158cbc894d542f911111009243bd4206831
LaMont Jones <lamont@mmjgroup.com>
2007-07-18 20:36:44 -0600

    2.12r-4ubuntu2

--- debian/hwclock.sh
+++ debian/hwclock.sh
@@ -16,10 +16,6 @@
 #		before changing this file. You risk serious clock
 #		misbehaviour otherwise.
 
-FIRST=no	# debian/rules sets this to 'yes' when creating hwclockfirst.sh
-
-[ "X$FIRST" = "Xyes" ] && AGAIN="" || AGAIN=" again"
-
 # Set this to any options you might need to give to hwclock, such
 # as machine hardware clock type for Alphas.
 HWCLOCKPARS=
@@ -36,12 +32,6 @@ hwclocksh()
     case "$UTC" in
        no|"")	GMT="--localtime"
 		UTC=""
-		if [ "X$FIRST" = "Xyes" ] && [ ! -r /etc/localtime ]; then
-		    if [ -z "$TZ" ]; then
-			log_action_msg "System clock was not updated at this time"
-			return 1
-		    fi
-		fi
 		;;
        yes)	GMT="--utc"
 		UTC="--utc"
@@ -61,54 +51,41 @@ hwclocksh()
 		echo "0.0 0 0.0" > /etc/adjtime
 	    fi
 
-	    if [ "$FIRST" != yes ]; then
-		# Uncomment the hwclock --adjust line below if you want
-		# hwclock to try to correct systematic drift errors in the
-		# Hardware Clock.
-		#
-		# WARNING: If you uncomment this option, you must either make
-		# sure *nothing* changes the Hardware Clock other than
-		# hwclock --systohc, or you must delete /etc/adjtime
-		# every time someone else modifies the Hardware Clock.
-		#
-		# Common "vilains" are: ntp, MS Windows, the BIOS Setup
-		# program.
-		#
-		# WARNING: You must remember to invalidate (delete)
-		# /etc/adjtime if you ever need to set the system clock
-		# to a very different value and hwclock --adjust is being
-		# used.
-		#
-		# Please read /usr/share/doc/util-linux/README.Debian.hwclock
-		# before enablig hwclock --adjust.
+	    # Uncomment the hwclock --adjust line below if you want
+	    # hwclock to try to correct systematic drift errors in the
+	    # Hardware Clock.
+	    #
+	    # WARNING: If you uncomment this option, you must either make
+	    # sure *nothing* changes the Hardware Clock other than
+	    # hwclock --systohc, or you must delete /etc/adjtime
+	    # every time someone else modifies the Hardware Clock.
+	    #
+	    # Common "vilains" are: ntp, MS Windows, the BIOS Setup
+	    # program.
+	    #
+	    # WARNING: You must remember to invalidate (delete)
+	    # /etc/adjtime if you ever need to set the system clock
+	    # to a very different value and hwclock --adjust is being
+	    # used.
+	    #
+	    # Please read /usr/share/doc/util-linux/README.Debian.hwclock
+	    # before enablig hwclock --adjust.
 
-		#hwclock --adjust $GMT $BADYEAR
-		:
-	    fi
+	    #hwclock --adjust $GMT $BADYEAR
+	    :
 
 	    if [ "$HWCLOCKACCESS" != no ]; then
-		log_daemon_msg "Setting the system clock$AGAIN"
+		[ "$VERBOSE" = no ] || log_daemon_msg "Setting the system clock"
 
 		# Copies Hardware Clock time to System Clock using the correct
 		# timezone for hardware clocks in local time, and sets kernel
 		# timezone. DO NOT REMOVE.
 		/sbin/hwclock --hctosys $GMT $HWCLOCKPARS $BADYEAR
 
-		if [ "$FIRST" = yes ]; then
-		    # Copies Hardware Clock time to System Clock using the correct
-		    # timezone for hardware clocks in local time, and sets kernel
-		    # timezone. DO NOT REMOVE.
-		    if [ -z "$TZ" ]; then
-		       /sbin/hwclock --noadjfile --hctosys $GMT $HWCLOCKPARS $BADYEAR
-		    else
-		       TZ="$TZ" /sbin/hwclock --noadjfile --hctosys $GMT $HWCLOCKPARS $BADYEAR
-		    fi
-
-		    if /sbin/hwclock --show $GMT $HWCLOCKPARS $BADYEAR 2>&1 > /dev/null |
-			grep -q '^The Hardware Clock registers contain values that are either invalid'; then
-			    echo "Invalid system date -- setting to 1/1/2002"
-			    /sbin/hwclock --set --date '1/1/2002 00:00:00' $GMT $HWCLOCKPARS $BADYEAR
-		    fi
+		if /sbin/hwclock --show $GMT $HWCLOCKPARS $BADYEAR 2>&1 > /dev/null |
+		    grep -q '^The Hardware Clock registers contain values that are either invalid'; then
+			echo "Invalid system date -- setting to 1/1/2002"
+			/sbin/hwclock --set --date '1/1/2002 00:00:00' $GMT $HWCLOCKPARS $BADYEAR
 		fi
 
 		#	Announce the local time.
@@ -116,12 +93,9 @@ hwclocksh()
 	    else
 		verbose_log_action_msg "Not setting System Clock"
 	    fi
-	    log_end_msg 0
+	    [ $VERBOSE = no ] || log_end_msg 0
 	    ;;
 	stop|restart|reload|force-reload)
-	    if [ "X$FIRST" = "Xyes" ]; then
-		return 0
-	    fi
 	    #
 	    # Updates the Hardware Clock with the System Clock time.
 	    # This will *override* any changes made to the Hardware Clock.
@@ -155,4 +129,4 @@ hwclocksh()
     esac
 }
 
-hwclocksh "$@"
+hwclocksh "$@" &
