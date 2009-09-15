521c4166b8cf388fd7bbc25d818d631db575a22e
Scott James Remnant <scott@ubuntu.com>
2009-09-15 15:16:31 +0100

    versionsort() prototype changed

--- include/strverscmp.h
+++ include/strverscmp.h
@@ -2,6 +2,6 @@
 #define STRVERSCMP_H
 
 extern int strverscmp (const char *s1, const char *s2);
-extern int versionsort (const void *a, const void *b);
+extern int version_sort (const void *a, const void *b);
 
 #endif
